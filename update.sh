24a598b545e3ec780550306f38a6b9d161379746
Adam Fowler <adamfowler71@gmail.com>
2024-09-18 11:04:08 +0100

    Update script changes

--- update.sh
+++ update.sh
@@ -8,14 +8,10 @@ function run_mustache {
     SRC=$1
     DEST=$2
     PROJECT=$3
+    TEMPLATE_CONTEXT=$4
 
-    if [[ -f "../$PROJECT.yml" ]]; then
-        CONTEXT="../$PROJECT.yml"
-    else
-        CONTEXT="../default.yml"
-    fi
     TEMP_FILE=$(mktemp)
-    echo "project: $PROJECT" | cat - $CONTEXT | mustache - "$SRC" > "$TEMP_FILE"
+    echo "project: $PROJECT" | cat - "$TEMPLATE_CONTEXT" | mustache - "$SRC" > "$TEMP_FILE"
     # delete file if it is empty or only contains spaces
     if ! grep -q '[^[:space:]]' "$TEMP_FILE" ; then
         rm "$TEMP_FILE"
@@ -33,6 +29,12 @@ function update_project {
 
     echo "Updating $PROJECT_NAME"
 
+    if [[ -f "$PROJECT_NAME.yml" ]]; then
+        TEMPLATE_CONTEXT="../$PROJECT_NAME.yml"
+    else
+        TEMPLATE_CONTEXT="../default.yml"
+    fi
+    
     pushd template > /dev/null
     for f in $(find . -print)
     do
@@ -41,7 +43,7 @@ function update_project {
             if [[ "$EXTENSION" == "sh" ]]; then
                 cp "$f" "$PROJECT_PATH"/"$f"
             elif [[ "$EXTENSION" == "mustache" ]]; then
-                run_mustache "$f" "$PROJECT_PATH"/"${f%.*}" "$PROJECT_NAME"
+                run_mustache "$f" "$PROJECT_PATH"/"${f%.*}" "$PROJECT_NAME" "$TEMPLATE_CONTEXT"
             else
                 cp "$f" "$PROJECT_PATH"/"$f"
             fi
